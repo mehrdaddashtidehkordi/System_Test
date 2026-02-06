0b096eddd316151e257e6e164b76177f786c46d7
mehrdaddashtidehkordi <mehrdaddashtidehkordi@gmail.com>
2026-02-06 22:12:40 +0100

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,6 @@
 terraform {
-  required_version = ">= 1.0"
+  required_version = ">= 1.0" 
 }
-
 resource "local_file" "devops_demo" {
   content  = "DevOps Infrastructure as Code"
   filename = "${path.module}/output.txt"
